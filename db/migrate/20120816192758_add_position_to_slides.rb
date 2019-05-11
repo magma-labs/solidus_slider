0a3270d1670fe415617ac6f35d64d454d5958275
Diego Mendoza <diego_saul_402@hotmail.com>
2019-05-10 19:59:53 -0500

    Change SolidusSupport by ActiveRecord

--- db/migrate/20120816192758_add_position_to_slides.rb
+++ db/migrate/20120816192758_add_position_to_slides.rb
@@ -1,4 +1,4 @@
-class AddPositionToSlides < SolidusSupport::Migration[4.2]
+class AddPositionToSlides < ActiveRecord::Migration[4.2]
   def change
     add_column :spree_slides, :position, :integer, null: false, default: 0
   end
--- db/migrate/20121219124126_add_product_id_to_slides.rb
+++ db/migrate/20121219124126_add_product_id_to_slides.rb
@@ -1,4 +1,4 @@
-class AddProductIdToSlides < SolidusSupport::Migration[4.2]
+class AddProductIdToSlides < ActiveRecord::Migration[4.2]
   def change
     add_column :spree_slides, :product_id, :integer
   end
